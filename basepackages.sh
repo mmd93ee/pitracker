0c59e1d73bb993c01567c9146d0f0dbcb64cc69b
Matthew Dutton <dutton.matt@gmail.com>
2019-02-06 19:08:14 +0000

    Created basepackages script

---
+++ basepackages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Update apt-get
+sudo apt-get -y update; sudo apt-get -y upgrade
+
+# Install baseline packages for system
+sudo apt-get install -y git 
+
+# Clone repo to pi
+cd ~
+git clone https://github.com/mmd93ee/pitracker.git
+cd pitracker
+
+
+# Powerup the hat
+./powerup_gsm.py
+
+
