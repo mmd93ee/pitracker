c4036f05aaa9f612e63c5edcff5a36f9f8bcaa70
Matthew Dutton <dutton.matt@gmail.com>
2019-02-06 19:02:28 +0000

    Created powerup python script

---
+++ powerup_gsm.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.out)
+
+while True:
+   GPIO.output(7, GPIO.LOW)
+   time.sleep(4)
+   GPIO.output(7. GPIO.HIGH)
+   break
+
+GPIO.cleanup()
+
